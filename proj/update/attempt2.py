ac7eb5ea5e8f7fe93c36b7470edc02462362b69a
RamiMatar <36045320+RamiMatar@users.noreply.github.com>
2023-02-11 22:38:48 -0500

    update

--- proj/update/attempt2.py
+++ proj/update/attempt2.py
@@ -1,3 +1,78 @@
+import torch
+import torch.nn as nn
+import torchaudio
+
+class Model(nn.Module):
+    def __init__(self, hparams):
+        super().__init__()
+        self.bandwidths = [int(bandwidth) for bandwidth in hparams['bandwidths'].split(',')]
+        self.n_mels = hparams['n_mels']
+        self.n_chroma = 12
+        self.N = hparams['bandwidth_freq_out_size']
+        self.K = len(self.bandwidths)
+        self.time_steps = hparams['time_steps']
+        self.sampling_rate = hparams['sampling_rate']
+        self.resampling_rate = hparams['resampling_rate']
+        self.batch_size = hparams['training_batch_size']
+        self.hop_length = hparams['hop_length']
+        self.segment_length = hparams['segment_length']
+        self.resampling_ratio = self.sampling_rate / self.resampling_rate
+        self.segment_samples = int(self.segment_length * self.sampling_rate)
+        self.segment_samples = int(self.segment_samples / self.hop_length) * self.hop_length
+        self.reserved = int(0.7 * self.batch_size * self.segment_samples + self.hop_length)
+        self.transforms = Transforms(self.sampling_rate, self.resampling_rate, hparams['n_fft'], self.hop_length, hparams['hop_length'], self.n_mels)
+        self.bandsplit = BandSplit(self.bandwidths[:], self.N, self.n_chroma, self.n_mels)
+        self.alternating_lstms = AlternatingBLSTMs(6, self.N, self.time_steps, self.K)
+        self.masks = MaskEstimation(self.bandwidths, self.N, hparams['training_batch_size'] * 2)
+
+    def forward(self, X):
+        X, chromas, mfccs = self.transforms(X)
+        X0 = X.permute(0,1,3,4,2)
+        X = self.bandsplit(X0, chromas, mfccs)
+        X = self.alternating_lstms(X)
+        X = self.masks(X)
+        X = X * X0
+        return X
+
+class Chroma(nn.Module):
+    def __init__(self, n_fft, sampling_rate, n_chroma = 12):
+        super().__init__()
+        self.n_fft = n_fft
+        self.sampling_rate = sampling_rate
+        self.n_chroma = n_chroma
+        freq_bins = torch.linspace(0, sampling_rate / 2, n_fft // 2 + 1)
+        mappings = self.semitone_mapping(freq_bins)
+        self.semitone_bins = []
+        for i in range(self.n_chroma):
+            self.semitone_bins.append(torch.argwhere(mappings == torch.Tensor([i])))
+    
+    def semitone_mapping(self, freq):
+        return self.n_chroma * torch.log2(freq / 440) % self.n_chroma
+
+    def forward(self, X):
+        chromas = []
+        for i in range(12):
+            chromas.append(torch.sum(X[:,:, self.semitone_bins[0],:], dim=2))
+        chromas = torch.cat(chromas, axis = 2)
+        return chromas
+
+class Transforms(nn.Module):
+    def __init__(self, input_freq = 44100, resample_freq = 16000, n_fft = 2048, hop_length = 1024, win_length=2048, n_mels = 32):
+        super().__init__()
+        self.stft = torchaudio.transforms.Spectrogram(n_fft = n_fft, hop_length = hop_length, win_length = win_length, power = None)
+        self.mel = torchaudio.transforms.MelScale(sample_rate = resample_freq, n_mels = n_mels, n_stft = n_fft // 2 + 1)
+        self.chroma = Chroma(n_fft = n_fft, sampling_rate = resample_freq)
+
+    def forward(self, X):
+        stft = self.stft(X)
+        power_spectrogram = torch.abs(stft).pow(2)
+        real = stft.real
+        imag = stft.imag
+        stft = torch.stack((real,imag), axis = 4)
+        stft = stft.permute(0,1,4,2,3)
+        mfccs= self.mel(power_spectrogram)
+        chromas = self.chroma(power_spectrogram)
+        return stft, chromas, mfccs
 class BandSplit(torch.nn.Module):
     # Input shape: torch.Size([16, 2, 1025, 431, 2])
     def __init__(self, bandwidths, N, n_chromas, n_mels):
